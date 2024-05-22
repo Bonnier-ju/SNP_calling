2f6047144962a87e5f01e8a85eb0e34320477adc
BONNIER Julien <julien.bonnier@inrae.fr>
2024-05-22 16:22:23 +0200

    Ajout de visualition des ACP pour les dimensions 1 à 5

--- 08.2-ACP_variables_env_Guyane.R
+++ 08.2-ACP_variables_env_Guyane.R
@@ -16,51 +16,14 @@ library(FactoMineR)
 library(factoextra)
 
 # Lire le fichier CSV
-file_path <- "C:/Users/bonni/OneDrive/Université/Thèse/Dicorynia/Article - Population Genomics/Analysis/08-EAA/Variables_climatiques_guyane/Variable_full_Guyane.csv"
+file_path <- "C:/Users/bonni/OneDrive/Université/Thèse/Dicorynia/Article - Population Genomics/Analysis/08-EAA/08.2-Variables_climatiques_guyane/Variable_full_Guyane.csv"
 data <- read_csv(file_path, locale = locale(encoding = "latin1"))
 
 # Supprimer les colonnes qui ne sont pas des variables environnementales
 data <- data %>%
   select(-wkt_geom, -fid)
 
-# Vérifier les premières lignes du dataframe pour s'assurer que les données ont été lues correctement
 head(data)
-
-# Réaliser l'ACP
-acp_result <- PCA(data, scale.unit = TRUE, ncp = 5, graph = FALSE)
-
-# Résumer les résultats de l'ACP
-print(acp_result)
-
-# Visualisation des variances expliquées par les composantes principales
-fviz_screeplot(acp_result, addlabels = TRUE, ylim = c(0, 50))
-
-# Visualisation des variables sur le plan factoriel
-fviz_pca_var(acp_result, col.var = "contrib", gradient.cols = c("#00AFBB", "#E7B800", "#FC4E07"), repel = TRUE)
-
-# Visualisation des individus sur le plan factoriel
-fviz_pca_ind(acp_result, geom.ind = "point", col.ind = "cos2", gradient.cols = c("#00AFBB", "#E7B800", "#FC4E07"), repel = TRUE)
-
-# Afficher les contributions des variables
-fviz_contrib(acp_result, choice = "var", axes = 1, top = 20)
-fviz_contrib(acp_result, choice = "var", axes = 2, top = 20)
-
-
-
-#########################################################################################
-# Charger les bibliothèques nécessaires
-library(readr)
-library(dplyr)
-library(ggplot2)
-library(FactoMineR)
-library(factoextra)
-
-# Lire les données à partir du fichier CSV
-file_path <- "C:/Users/bonni/OneDrive/Université/Thèse/Dicorynia/Article - Population Genomics/Analysis/08-EAA/08.2-Variables_climatiques_guyane/Variable_full_Guyane.csv"
-data <- read_csv(file_path)
-
-# Afficher les premières lignes des données pour comprendre leur structure
-print(head(data))
 
 # Convertir les variables catégorielles en variables numériques (encodage one-hot)
 data <- data %>%
@@ -71,28 +34,39 @@ data <- data %>%
 data_scaled <- scale(data)
 
 # Réaliser l'ACP
-pca_result <- PCA(data_scaled, graph = FALSE)
+acp_result <- PCA(data, scale.unit = TRUE, ncp = 5, graph = FALSE)
+print(acp_result)
 
-# Visualiser les résultats de l'ACP
-# Biplot des individus et des variables
-fviz_pca_biplot(pca_result, repel = TRUE, col.var = "blue", col.ind = "red") +
-  ggtitle("Biplot des individus et des variables")
+# Visualisation des variances expliquées par les composantes principales
+fviz_screeplot(acp_result, addlabels = TRUE, ylim = c(0, 50))
 
-# Variance expliquée par chaque composante principale
-fviz_screeplot(pca_result, addlabels = TRUE, ylim = c(0, 50)) +
-  ggtitle("Variance expliquée par chaque composante principale")
+# Visualisation des variables sur le plan factoriel 1 et 2
+fviz_pca_var(acp_result, axes = c(1, 2), col.var = "contrib", gradient.cols = c("#00AFBB", "#E7B800", "#FC4E07"), repel = TRUE) +
+  ggtitle("ACP - Dimensions 1 et 2")
 
-# Contributions des variables à la première composante principale
-fviz_pca_var(pca_result, col.var = "contrib") +
-  scale_color_gradient2(low = "white", mid = "blue", high = "red", midpoint = 50) +
-  theme_minimal() +
-  ggtitle("Contributions des variables à la première composante principale")
+# Visualisation des variables sur le plan factoriel 1 et 3
+fviz_pca_var(acp_result, axes = c(1, 3), col.var = "contrib", gradient.cols = c("#00AFBB", "#E7B800", "#FC4E07"), repel = TRUE) +
+  ggtitle("ACP - Dimensions 1 et 3")
 
-# Contributions des individus à la première composante principale
-fviz_pca_ind(pca_result, col.ind = "cos2") +
-  scale_color_gradient2(low = "white", mid = "blue", high = "red", midpoint = 0.5) +
-  theme_minimal() +
-  ggtitle("Contributions des individus à la première composante principale")
+# Visualisation des variables sur le plan factoriel 2 et 3
+fviz_pca_var(acp_result, axes = c(2, 3), col.var = "contrib", gradient.cols = c("#00AFBB", "#E7B800", "#FC4E07"), repel = TRUE) +
+  ggtitle("ACP - Dimensions 2 et 3")
+
+# Visualisation des variables sur le plan factoriel 2 et 4
+fviz_pca_var(acp_result, axes = c(2, 4), col.var = "contrib", gradient.cols = c("#00AFBB", "#E7B800", "#FC4E07"), repel = TRUE) +
+  ggtitle("ACP - Dimensions 2 et 4")
+
+# Visualisation des variables sur le plan factoriel 4 et 5
+fviz_pca_var(acp_result, axes = c(4, 5), col.var = "contrib", gradient.cols = c("#00AFBB", "#E7B800", "#FC4E07"), repel = TRUE) +
+  ggtitle("ACP - Dimensions 4 et 5")
+
+# Afficher les contributions des variables
+fviz_contrib(acp_result, choice = "var", axes = 1, top = 20)
+fviz_contrib(acp_result, choice = "var", axes = 2, top = 20)
+fviz_contrib(acp_result, choice = "var", axes = 3, top = 20)
+fviz_contrib(acp_result, choice = "var", axes = 4, top = 20)
+fviz_contrib(acp_result, choice = "var", axes = 5, top = 20)
+
 
 
 
